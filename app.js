675b250eb04cd6215539d14eb8a20e6676c15b26
Takaya Seiwa <ljks1021@gmail.com>
2021-11-15 17:56:26 +0900

    getElementById実装

---
+++ app.js
@@ -0,0 +1,12 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+    'スーパーファミコン',
+    'PS2',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+console.log();
+
+document.getElementById('js-question').textContent = 'Question';
